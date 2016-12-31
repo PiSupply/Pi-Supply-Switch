43498607486dc0fd19abf50980abdefd81f68cf4
Garage Tech <francesco.vannini@happylot.net>
2016-12-31 18:24:49 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [ "$(id -u)" != "0" ]; then  #Check if script is being run as root
+#Check if script is being run as root
+if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
+
 if [ ! $? = 0 ]; then
    exit 1
 else
-   cd /home/pi
    git clone -b patch-1 https://github.com/francesco-vannini/Pi-Supply-Switch.git
-   mkdir "/opt/piswitch"
-   cp -a Pi-Supply-Switch/softshut.py /opt/piswitch
-   cp -a Pi-Supply-Switch/piswitch.service /etc/systemd/system
+   mkdir /opt/piswitch
+   cp Pi-Supply-Switch/softshut.py /opt/piswitch
+   cp Pi-Supply-Switch/piswitch.service /etc/systemd/system
 
    systemctl enable /etc/systemd/system/piswitch.service
    systemctl start piswitch.service
