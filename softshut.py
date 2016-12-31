f162ee87d7c199f55959a3077b284e6c0e07d444
Garage Tech <francesco.vannini@happylot.net>
2016-12-31 11:32:14 +0000

    Update softshut.py

--- softshut.py
+++ softshut.py
@@ -1,19 +1,18 @@
+#!/usr/bin/env python
+
 # Import the modules to send commands to the system and access GPIO pins
 from subprocess import call
-import RPi.GPIO as gpio
-import time
+import RPi.GPIO as GPIO
 
-# Define a function to keep script running
-def loop():
-    while 1:
-        time.sleep(5)
+# Map pin seven and eight on the Pi Switch PCB to chosen pins on the Raspberry Pi header
+# The PCB numbering is a legacy with the original design of the board
+PinSeven = 7
+PinEight = 11
+GPIO.setmode(GPIO.BOARD) # Set pin numbering to board numbering
+GPIO.setup(PinSeven, GPIO.IN) # Set up PinSeven as an input
+GPIO.setup(PinEight, GPIO.OUT, initial=1) # Setup PinEight as output
 
-# Define a function to run when an interrupt is called
-def shutdown(pin):
-    call('poweroff', shell=False)
+GPIO.wait_for_edge(PinSeven, GPIO.RISING) # Wait for a rising edge on PinSeven
 
-gpio.setmode(gpio.BOARD) # Set pin numbering to board numbering
-gpio.setup(7, gpio.IN) # Set up pin 7 as an input
-gpio.add_event_detect(7, gpio.RISING, callback=shutdown, bouncetime=200) # Set up an interrupt to look for button presses
-
-loop() # Run the loop function to keep script running
+GPIO.output(PinEight,0) # Bring down PinEight so that the capacitor can discharge an remove power to the Pi
+call('poweroff', shell=False) # Initiate OS Poweroff
