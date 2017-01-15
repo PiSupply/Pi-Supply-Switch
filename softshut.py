1c9adddcd9fa595d92d3174c4dc3993a49b1228b
Garage Tech <francesco.vannini@happylot.net>
2017-01-15 16:52:56 +0000

    Update softshut.py
    Added a reboot functionality

--- softshut.py
+++ softshut.py
@@ -16,6 +16,11 @@ GPIO.setup(PinEight, GPIO.OUT, initial=1) # Setup PinEight as output
 while (GPIO.input(PinSeven) == False): # While button not pressed
  GPIO.wait_for_edge(PinSeven, GPIO.RISING) # Wait for a rising edge on PinSeven
  sleep(0.1); # Sleep 100ms to avoid triggering a shutdown when a spike occured
+ 
+sleep(2); # Sleep 2s to distinguish a long press from a short press
 
-GPIO.output(PinEight,0) # Bring down PinEight so that the capacitor can discharge an remove power to the Pi
-call('poweroff', shell=False) # Initiate OS Poweroff
+if (GPIO.input(PinSeven) == False):
+ GPIO.output(PinEight,0) # Bring down PinEight so that the capacitor can discharge and remove power to the Pi
+ call('poweroff', shell=False) # Initiate OS Poweroff
+else:
+ call('reboot', shell=False) # Initiate OS Reboot
